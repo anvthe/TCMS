d3c186ce5d0427f6c1fb9d7a48f0048486ecdae3
Roney <guddorx@gmail.com>
2024-09-30 11:11:15 +0600

    commit from roney

--- src/main/java/cns/tcms/TcmsApplication.java
+++ src/main/java/cns/tcms/TcmsApplication.java
@@ -2,8 +2,11 @@ package cns.tcms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class TcmsApplication {
 
 	public static void main(String[] args) {
@@ -11,4 +14,8 @@ public class TcmsApplication {
 		System.out.println("Toll Collection Management System is running =====================================");
 	}
 
+	@GetMapping
+	public String hello() {
+		return "Hello World";
+	}
 }
